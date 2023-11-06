efc5d06967176d6209b9d29acc1643bf571971bd
Sheyvie <sheilayvi@gmail.com>
2023-11-06 21:22:08 +0300

    second Commit

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ var removeElement = function (nums, val) {
   return k; // k is the number of elements not equal to val.
 };
 
-// Example usage:
+// Examples
 const nums1 = [3, 2, 2, 3];
 const val1 = 3;
 const k1 = removeElement(nums1, val1);
-console.log(k1); // Output: 2
-console.log(nums1); // Output: [2, 2, _, _]
+console.log(k1);
+console.log(nums1);
 
 const nums2 = [0, 1, 2, 2, 3, 0, 4, 2];
 const val2 = 2;
 const k2 = removeElement(nums2, val2);
-console.log(k2); // Output: 5
-console.log(nums2); // Output: [0, 1, 4, 3, 0, _, _, _]
+console.log(k2);
+console.log(nums2);
